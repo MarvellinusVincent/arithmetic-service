a268beda606147b4d9e71e0e2cbff2faa98a897e
Marvell <vmarvellinus@gmail.com>
2024-04-04 17:00:37 -0400

    changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
+require ("dotenv").config()
 const express = require('express');
 const cors = require('cors');
 const { add } = require("./arithmetica");
 const app = express();
 app.use(cors());
+
+if (!process.env.PORT) {
+  throw new Error ("Please specify the port number for the HTTP server with the environment variable PORT")
+}
+
 const port = 3000;
 app.get('/', (req, res) => {
-    res.send('Arithmetic Service - Last Updated Februrary 15 2024');
+    res.send('Arithmetic Service - Last Updated March 5 2024');
 });
 app.get('/add/:n/:m', (req, res) => {
     let n = Number(req.param.n);
